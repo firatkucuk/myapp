dc608595dbc9ec467268fe24b5413e2264f747d6
Firat Kucuk <firat.kucuk@doculayer.com>
2022-07-08 21:01:04 +0200

    failing unit tests

--- src/test/java/com/example/myapp/MyappApplicationTests.java
+++ src/test/java/com/example/myapp/MyappApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.myapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,6 @@ class MyappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(1 == 2);
 	}
-
 }
